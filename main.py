cb645b93bebf2afabe042c5de050ea8e90a5251c
furubese <furubese@gmail.com>
2020-05-10 19:43:46 +0900

    update:コッホ曲線！

--- main.py
+++ main.py
@@ -12,59 +12,56 @@ class Draw:
         print("==self.p==")
         print(self.p)
         x, y = 0, 1
-        sa = self.p[0][x]
+        sa = self.p[0][x], self.p[0][y]
         ln = (self.p[1][x] - self.p[0][x]) / 2
         N1 = ln / (1 + math.sin(kakudo / 2))
         N2 = ln * 2 - N1
-        tate = math.cos(kakudo / 2) * N1
-        print("yaaaaptate={}".format(tate))
+        tate = math.cos(kakudo / 2) * N1 + sa[y]
         p.append([self.p[0][x], self.p[0][y]])
-        p.append([N1 + sa, 0.0])
-        p.append([ln + sa, tate])
-        p.append([N2 + sa, 0.0])
+        p.append([N1 + sa[x], sa[y]])
+        p.append([ln + sa[x], tate])
+        p.append([N2 + sa[x], sa[y]])
         p.append([self.p[1][x], self.p[1][y]])
         self.rp = p
         print("==bunkatu==")
         print("rp = {}".format(self.rp))
 
-    def cyck(self, kakudo, *plist):
+    def cyck(self, kakudo, plist):
         p = []
         x, y = 0, 1
-        for i in range(len(*plist)):
-            p.append([self.rp[i][x] * math.cos(kakudo) - self.rp[i][y] * math.sin(kakudo),
-                      self.rp[i][x] * math.sin(kakudo) + self.rp[i][y] * math.cos(kakudo)])
+        for i in range(len(plist)):
+            p.append([plist[i][x] * math.cos(kakudo) - plist[i][y] * math.sin(kakudo),
+                      plist[i][x] * math.sin(kakudo) + plist[i][y] * math.cos(kakudo)])
         print("==cyck==")
         print(p)
         return p
 
-    def de_cyck(self, kakudo, *plist):
+    def de_cyck(self, kakudo, plist):
         p = []
         x, y = 0, 1
-        for i in range(len(*plist)):
-            p.append([self.p[i][x] * math.cos(kakudo) + self.p[i][y] * math.sin(kakudo),
-                      - self.p[i][x] * math.sin(kakudo) + self.p[i][y] * math.cos(kakudo)])
+        for i in range(len(plist)):
+            p.append([plist[i][x] * math.cos(kakudo) + plist[i][y] * math.sin(kakudo),
+                      - plist[i][x] * math.sin(kakudo) + plist[i][y] * math.cos(kakudo)])
         print("==de_cyck==")
         print(p)
         return p
 
     def sai(self, kakudo):
         sp = []
-        try:
-            i = 0
-            while i < len(self.rp):
-                sp.append(self.rp[i])
-                p = [self.rp[i], self.rp[i + 1]]
-                rps = Draw(p)
-                kaku = math.atan((self.rp[i + 1][1] - self.rp[i][1]) / (self.rp[i + 1][0] - self.rp[i][0]))
-                rps.p = rps.de_cyck(kaku, rps.p[0])
-                rps.bunkatu(kakudo)
-                rps.rp = rps.cyck(kaku, rps.rp)
-                sp.extend(rps.rp)
-                sp.append(self.rp[i + 1])
-                print("sp = {}".format(sp))
-                i += 1
-        except IndexError:
-            pass
+        i = 0
+        while i + 1 < len(self.rp):
+            sp.append(self.rp[i])
+            kaku = math.atan((self.rp[i + 1][1] - self.rp[i][1]) / (self.rp[i + 1][0] - self.rp[i][0]))
+            print("kaku = {}".format(kaku))
+            p = [self.rp[i], self.rp[i + 1]]
+            rps = Draw(p)
+            rps.p = rps.de_cyck(kaku, rps.p)
+            rps.bunkatu(kakudo)
+            rps.rp = rps.cyck(kaku, rps.rp)
+            sp.extend(rps.rp)
+            sp.append(self.rp[i + 1])
+            print("sp = {}".format(sp))
+            i += 1
         print("==sp==")
         print(sp)
         self.rp = sp
@@ -81,8 +78,9 @@ class Draw:
         plt.plot(x, y)
         plt.show()
 
+
 if __name__ == "__main__":
-    point = [0.0, 0.0], [1.0, 0.0]
+    point = [0.0, 1.0], [1.0, 1.0]
     dl = Draw(point)
     dl.bunkatu(math.pi / 6)
     dl.sai(math.pi / 6)
