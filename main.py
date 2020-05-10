ef72aa54fa90737b2ae7c86d4dfa6b1344c2b2a4
furubese <furubese@gmail.com>
2020-05-10 23:42:38 +0900

    update:奥行みたいな感じに

--- main.py
+++ main.py
@@ -69,6 +69,7 @@ class Draw:
             sp.extend(rps.rp[0:-1])
             prinz("sp = {}".format(sp), self.debug)
             i += 1
+        prinz("self.rp = {}".format(self.rp), self.debug)
         sp.append(self.rp[-1])
         self.rp = sp
 
@@ -83,6 +84,23 @@ class Draw:
             for i in range(len(cyd)):
                 sp.append([cyd[i][x] + self.rp[-1][x], cyd[i][y] + self.rp[-1][y]])
             prinz("===cop===", self.debug)
+            prinz(sp, self.debug)
+            self.rp.extend(sp)
+
+    def spril(self, kakudo, poi, k):
+        p = [[0.0, 0.0], [poi[0], poi[1]]]
+        for g in range(100):
+            p = [[0.0, 0.0], [p[1][0] * 0.99, p[1][1]]]
+            dle = Draw(p, False)
+            dle.rp = p
+            for i in range(k):
+                dle.sai(kakudo)
+                print("{}.".format(i))
+            cyd = Draw(0).cyck((math.pi / 1 * 3 + kakudo) * (g + 1), dle.rp)
+            sp = []
+            for i in range(len(dle.rp)):
+                sp.append([cyd[i][0] + self.rp[-1][0], cyd[i][1] + self.rp[-1][1]])
+            prinz("===spril===", self.debug)
             prinz(sp, self.debug)
             self.rp.extend(sp)
 
@@ -101,11 +119,13 @@ class Draw:
 
 if __name__ == "__main__":
     point = [0.0, 0.0], [1.0, 0.0]
+    kei = 2
     kakus = math.pi * 1 / 180
-    dl = Draw(point, True)
+    dl = Draw(point, False)
     dl.rp = point
-    for i in range(1):
-        dl.sai(kakus * 36)
+    for i in range(kei):
+        dl.sai(kakus * 60)
         print("{}.".format(i))
-    dl.cop(kakus * 36, 4)
+    dl.cop(kakus * 60, 2)
+    dl.spril(kakus * 60, [point[1][0], point[1][1]], kei)
     dl.draw()
