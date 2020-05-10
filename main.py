314a11d82904a2ae43d4069fe26bb6c33f5f1fe4
furubese <furubese@gmail.com>
2020-05-10 18:30:00 +0900

    update:

--- main.py
+++ main.py
@@ -1,76 +1,89 @@
 import matplotlib.pyplot as plt
 import math
 
+class Draw:
+    def __init__(self, p):
+        self.p = p
+        self.sp = p
+        self.rp = []
 
-def kaku(x, y, waru, sita, k):
-    a = []
-    b = []
-    wanlen = (x[1] - x[0]) / waru
-    for i in range(waru + 1):
-        b.append(y[0])
-    for i in range(waru + 1):
-        a.append(x[0] + wanlen * i)
-    a.insert(2, kaku2(a[1:-1], b[1:-1]))
-    b.insert(2, wanlen * math.sin(math.pi * k / 180))
-    i = 0
-    ra = []
-    rb = []
-    while i < len(a):
-        ra.append(a[i] * math.cos(sita) - b[i] * math.sin(sita))
-        rb.append(a[i] * math.sin(sita) + b[i] * math.cos(sita))
-        i += 1
-    print(a, b)
-    print(math.cos(sita))
-    return ra, rb
+    def bunkatu(self, kakudo):
+        p = []
+        print("==self.p==")
+        print(self.p)
+        x, y = 0, 1
+        sa = self.p[0][x]
+        ln = (self.p[1][x] - self.p[0][x]) / 2
+        N1 = ln / (1 + math.sin(kakudo / 2))
+        N2 = ln * 2 - N1
+        tate = math.cos(kakudo / 2) * N1
+        print("yaaaaptate={}".format(tate))
+        p.append([self.p[0][x], self.p[0][y]])
+        p.append([N1 + sa, 0.0])
+        p.append([ln + sa, tate])
+        p.append([N2 + sa, 0.0])
+        p.append([self.p[1][x], self.p[1][y]])
+        self.rp = p
+        print("==bunkatu==")
+        print("rp = {}".format(self.rp))
 
+    def cyck(self, kakudo, *plist):
+        p = []
+        x, y = 0, 1
+        for i in range(len(*plist)):
+            p.append([self.rp[i][x] * math.cos(kakudo) - self.rp[i][y] * math.sin(kakudo),
+                      self.rp[i][x] * math.sin(kakudo) + self.rp[i][y] * math.cos(kakudo)])
+        print("==cyck==")
+        print(p)
+        return p
 
-def kaku2(x, y):
-    a = []
-    for i in range(2 + 1):
-        a.append((x[1] - x[0]) / 2 * i + x[0])
-    return a[1]
+    def de_cyck(self, kakudo, *plist):
+        p = []
+        x, y = 0, 1
+        for i in range(len(*plist)):
+            p.append([self.p[i][x] * math.cos(kakudo) + self.p[i][y] * math.sin(kakudo),
+                      - self.p[i][x] * math.sin(kakudo) + self.p[i][y] * math.cos(kakudo)])
+        print("==de_cyck==")
+        print(p)
+        return p
 
+    def sai(self, kakudo):
+        sp = []
+        try:
+            i = 0
+            while i < len(self.rp):
+                sp.append(self.rp[i])
+                p = [self.rp[i], self.rp[i + 1]]
+                rps = Draw(p)
+                kaku = math.atan((self.rp[i + 1][1] - self.rp[i][1]) / (self.rp[i + 1][0] - self.rp[i][0]))
+                rps.p = rps.de_cyck(kaku, rps.p[0])
+                rps.bunkatu(kakudo)
+                rps.rp = rps.cyck(kaku, rps.rp)
+                sp.extend(rps.rp)
+                sp.append(self.rp[i + 1])
+                print("sp = {}".format(sp))
+                i += 1
+        except IndexError:
+            pass
+        print("==sp==")
+        print(sp)
+        self.rp = sp
+        print("==sai==")
+        print("rp = {}".format(self.rp))
 
-def kaku3(x, y, waru, sita, k):
-    """TODO 全部書き直し"""
-    ra = []
-    rb = []
-    i = 0
-    while i < len(x):
-        tmp = kaku([x[i], x[i+1]],
-                       [y[i], y[i+1]], waru, sita, k)
-        ra.append(x[i])
-        ra.extend(tmp[0])
-        ra.append(x[i+1])
-        rb.append(y[i])
-        rb.extend(tmp[1])
-        rb.append(y[i + 1])
-        i += 2
-
+    def draw(self):
+        x = []
+        y = []
+        for i in self.rp:
+            x.append(i[0])
+            y.append(i[1])
+        print("x = {}\ny = {}".format(x, y))
+        plt.plot(x, y)
+        plt.show()
 
 if __name__ == "__main__":
-    xz = [0.0, 1.0]
-    yz = [0.0, 0.0]
-    gx = 0
-    gy = 0
-    tmp_x = []
-    tmp_y = []
-    rx = []
-    ry = []
-    w = 3
-    kakudo = 90
-    kakudo2 = math.pi * 216 / 180
-    for i in range(5):
-        tmp_x = (kaku(xz, yz, w, kakudo2 * i, kakudo)[0])
-        tmp_y = (kaku(xz, yz, w, kakudo2 * i, kakudo)[1])
-        for g in range(len(tmp_x)):
-            tmp_x[g] = tmp_x[g] + gx
-            tmp_y[g] = tmp_y[g] + gy
-        gx, gy = tmp_x[-1], tmp_y[-1]
-        rx.extend(tmp_x)
-        ry.extend(tmp_y)
-
-    print(xz, yz)
-    print("x = {}\ny = {}".format(rx, ry))
-    plt.plot(rx, ry)
-    plt.show()
+    point = [0.0, 0.0], [1.0, 0.0]
+    dl = Draw(point)
+    dl.bunkatu(math.pi / 6)
+    dl.sai(math.pi / 6)
+    dl.draw()
